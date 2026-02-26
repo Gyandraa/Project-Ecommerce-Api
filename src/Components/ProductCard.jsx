ab172d2069da6762ab5b4cbf0ebadadc9c9c85af
gyandra <gyandranaupal14@gmail.com>
2026-02-26 15:45:16 +0700

    fix: change grid column for responsive in mobile

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,6 +1,6 @@
 export default function ProductCard({ products }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-10">
       {products.map((product) => (
         <div
           key={product.id}
