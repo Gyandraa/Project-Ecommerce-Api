cbb1eb2ee0d15262b03de7027dfe2d4cc3fccd6f
gyandra <gyandranaupal14@gmail.com>
2026-03-07 15:11:07 +0700

    feat: mapping for productByCategories and fixed useparams

--- src/Hooks/useProductCategories.jsx
+++ src/Hooks/useProductCategories.jsx
@@ -1,1 +1,18 @@
-export default function useProdukCategories() {}
+import { useQuery } from "@tanstack/react-query";
+import { getProductByCategories } from "../Service/productService";
+
+export default function useProdukCategories(category) {
+  const { data, isPending, isError } = useQuery({
+    queryKey: ["productbyCategory", category],
+    queryFn: () => getProductByCategories(category),
+    staleTime: 1000 * 60 * 5,
+    refetchOnWindowFocus: false,
+    retry: 3,
+  });
+
+  return {
+    productByCategory: data ?? null,
+    isPending,
+    isError,
+  };
+}
