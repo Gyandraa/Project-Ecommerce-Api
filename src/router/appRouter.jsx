efc0bc0a1f28c7de1f857b50f9218efac3625074
gyandra <gyandranaupal14@gmail.com>
2026-03-07 15:12:54 +0700

    fix(router): change name in routes for category product

--- src/router/appRouter.jsx
+++ src/router/appRouter.jsx
@@ -1,13 +1,17 @@
 import HomePages from "../Pages/Home";
 import { Routes, Route } from "react-router-dom";
 import ProductDetail from "../Components/product/ProductDetail";
-
+import ProductCategory from "../Components/category/ProductCategory";
 export default function Router() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePages />} />
         <Route path="/product/:id" element={<ProductDetail />} />
+        <Route
+          path="/product/category/:category"
+          element={<ProductCategory />}
+        />
       </Routes>
     </>
   );
