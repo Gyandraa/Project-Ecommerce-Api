ea9381a8569efa638b5480a37c19ba71de9e436d
gyandra <gyandranaupal14@gmail.com>
2026-03-10 04:54:19 +0700

    style: styling cart item for be a better

--- src/Components/cart/CartItem.jsx
+++ src/Components/cart/CartItem.jsx
@@ -1,38 +1,43 @@
 export default function CartItem({ cart }) {
-  if (!cart || cart.length === 0) {
-    return <p>Cart kosong</p>;
+  if (cart.length === 0) {
+    return (
+      <p className="text-center mt-5 text-xl font-semibold">Cart Kosong</p>
+    );
   }
-
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-20 gap-6 mt-40">
+    <div className="flex flex-col mx-20 gap-6 mt-20">
       {cart.map((item) => (
-        <div key={item.id}>
-          <div
-            className="
-            bg-white rounded-2xl p-4
-            shadow-sm hover:shadow-xl
-            transition-all duration-300
-            hover:-translate-y-2
-            cursor-pointer
-            flex flex-col h-full
-          "
-          >
-            <div className="h-52 w-full flex items-center justify-center overflow-hidden mb-2">
-              <img
-                src={item.thumbnail}
-                loading="lazy"
-                decoding="async"
-                alt={item.title}
-                className="w-full h-full object-contain transition-transform duration-300 hover:scale-110"
-              />
-            </div>
+        <div
+          key={item.id}
+          className="
+        bg-white rounded-2xl p-4
+        shadow-sm hover:shadow-xl
+        transition-all duration-300
+        hover:-translate-y-2
+        cursor-pointer
+        flex items-center gap-6
+      "
+        >
+          {/* Gambar */}
+          <div className="w-32 h-32 flex-shrink-0 overflow-hidden">
+            <img
+              src={item.thumbnail}
+              loading="lazy"
+              decoding="async"
+              alt={item.title}
+              className="w-full h-full object-contain transition-transform duration-300 hover:scale-110"
+            />
+          </div>
 
-            <h2 className="text-2xl  text-center font-semibold text-gray-800 line-clamp-2 min-h-[55px]">
+          {/* Text */}
+          <div className="flex flex-col justify-center flex-1">
+            <h2 className="text-xl font-semibold text-gray-800 line-clamp-2">
               {item.title}
             </h2>
-            <h2 className="text-2xl  text-center font-semibold text-gray-800 line-clamp-2 min-h-[55px]">
-              {item.price}
-            </h2>
+
+            <p className="text-lg font-bold text-pink-600 mt-2">
+              ${item.price}
+            </p>
           </div>
         </div>
       ))}
