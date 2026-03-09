f2f1df8e5c4cefe6e82b5cf89dcd9486b0c15b36
gyandra <gyandranaupal14@gmail.com>
2026-03-10 04:52:28 +0700

    feat: add fitur for cart length

--- src/Components/layout/navbar.jsx
+++ src/Components/layout/navbar.jsx
@@ -1,39 +1,37 @@
 import { Link } from "react-router-dom";
-
+import UseCart from "../../Hooks/useCart";
 export default function Navbar() {
+  const { cart } = UseCart();
   return (
     <nav className="fixed top-0 left-0 w-full bg-white shadow-xl z-50">
       <div className="max-w-7xl mx-auto flex justify-between items-center px-6 py-4">
-        <h1 className="text-2xl font-bold font-sans text-gray-800">
-          Dibelanjain
-        </h1>
+        <h1 className="text-2xl font-bold text-gray-800">Dibelanjain</h1>
 
-        <div className="hidden md:flex gap-8 font-medium text-gray-600">
-          <input
-            type="text"
-            placeholder="Search product..."
-            className=" w-100
-            
-    px-4 py-2
-    rounded-full
-    border border-gray-300
-    focus:outline-none
-    focus:ring-2 focus:ring-orange-500
-    transition
-    shadow-sm
-    placeholder:text-gray-400
-  "
-          />
-        </div>
+        <div className="flex gap-5 items-center">
+          <Link to="/cart" className="relative">
+            <img
+              className="w-10 h-10 rounded-full cursor-pointer hover:scale-110 transition"
+              src="/assets/keranjang.jpg"
+              alt="keranjang"
+            />
 
-        <div className="flex gap-5">
+            {cart.length > 0 && (
+              <span
+                className="
+                absolute -top-2 -right-2
+                bg-red-500 text-white
+                text-xs font-bold
+                px-2 py-1
+                rounded-full
+              "
+              >
+                {cart.length}
+              </span>
+            )}
+          </Link>
+
           <img
-            className="w-10 h-10 rounded-full cursor-pointer hover:scale-130 transition duration-300"
-            src="/assets/keranjang.jpg"
-            alt="keranjang"
-          />
-          <img
-            className="w-10 h-10 rounded-full cursor-pointer hover:scale-130 transition duration-300"
+            className="w-10 h-10 rounded-full cursor-pointer hover:scale-110 transition"
             src="/assets/logo-user.jpg"
             alt="user"
           />
