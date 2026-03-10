c643349a1623eb92489de7f4b2858cce5dfd3b7a
gyandra <gyandranaupal14@gmail.com>
2026-03-10 21:18:50 +0700

    add function for fiture deleteProduct

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -14,8 +14,15 @@ export default function UseCart() {
     setCart((prev) => [...prev, product]);
   };
 
+  const deleteProduct = (id) => {
+    const updatedProduct = cart.filter((item) => item.id !== id);
+    setCart(updatedProduct);
+    localStorage.setItem("cart", JSON.stringify(updatedProduct));
+  };
+
   return {
     cart,
     addProduct,
+    deleteProduct,
   };
 }
