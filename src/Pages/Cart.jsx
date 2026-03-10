42fb31917c59b68ea417bf566004333036808bb1
gyandra <gyandranaupal14@gmail.com>
2026-03-10 21:14:56 +0700

    add props for caritem fiture delete product

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,13 +1,13 @@
 import CartItem from "../Components/cart/cartItem";
 import UseCart from "../Hooks/useCart";
 export default function CartPages() {
-  const { cart } = UseCart();
+  const { cart, deleteProduct } = UseCart();
   return (
     <div>
       <h2 className="mt-30 font-semibold font-mono text-2xl text-center">
         Cart Pages
       </h2>
-      <CartItem cart={cart} />
+      <CartItem cart={cart} deleteProduct={deleteProduct} />
     </div>
   );
 }
