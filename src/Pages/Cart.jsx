a03afba76973ca460db71f50847066d13c418e26
gyandra <gyandranaupal14@gmail.com>
2026-03-12 00:12:31 +0700

    feat: change import from usecart to cartcontext

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,13 +1,17 @@
 import CartItem from "../Components/cart/cartItem";
-import UseCart from "../Hooks/useCart";
+import { UseCart } from "../context/CartContext";
 export default function CartPages() {
-  const { cart, deleteProduct } = UseCart();
+  const { cart, deleteProduct, addquantity } = UseCart();
   return (
     <div>
       <h2 className="mt-30 font-semibold font-mono text-2xl text-center">
         Cart Pages
       </h2>
-      <CartItem cart={cart} deleteProduct={deleteProduct} />
+      <CartItem
+        cart={cart}
+        deleteProduct={deleteProduct}
+        addquantit={addquantity}
+      />
     </div>
   );
 }
