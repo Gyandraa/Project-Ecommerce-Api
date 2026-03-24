f6ba9bf495aa9dc81ad357ebaeabd8e3e17a1508
gyandra <gyandranaupal14@gmail.com>
2026-03-24 20:14:59 +0700

    add

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,4 +1,4 @@
-import CartItem from "../Components/cart/cartItem";
+import CartItem from "../Components/cart/cartItem.jsx";
 import { UseCart } from "../context/CartContext";
 export default function CartPages() {
   const { cart, deleteProduct, addQuantity, deleteQuantity, totalPrice } =
