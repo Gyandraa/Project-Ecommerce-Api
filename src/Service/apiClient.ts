dba13caeb09ddb64eb9801c4ea0de1744c0401bc
gyandra <gyandranaupal14@gmail.com>
2026-03-28 12:16:32 +0700

    feat: change call api from fetch to axios'

--- src/Service/apiClient.ts
+++ src/Service/apiClient.ts
@@ -1,13 +1,16 @@
+import axios from "axios";
+
 const BASE_URL = "https://dummyjson.com";
 
 export async function apiClient(endpoint: string) {
   try {
-    const response = await fetch(`${BASE_URL}${endpoint}`);
+    const response = await axios.get(`${BASE_URL}${endpoint}`);
 
-    if (!response.ok) {
-      throw new Error("Network response failed");
+    if (response.status !== 200) {
+      throw new Error(`API request failed with status ${response.status}`);
     }
-    return await response.json();
+
+    return await response.data();
   } catch (error) {
     console.error("API CLIENT ERROR:", error);
     return null;
